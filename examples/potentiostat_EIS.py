3d43d493e71652ecde0102c41080a71c57b595eb
louisr <l.regnacq@gmail.com>
2021-12-09 12:14:27 +0100

    add example scripts

--- examples/potentiostat_EIS.py
+++ examples/potentiostat_EIS.py
@@ -10,25 +10,24 @@
 		Analysis_Instrument - class handling Analog Discovery 2 (Digilent)
 
 """
-
-
 import bimms as bm
 import numpy as np
 import matplotlib.pyplot as plt
 
-fmin = 1000
-fmax = 10e6
-n_pts = 200
-amp = 0.1
-settling_time = 0.01
-NPeriods = 32
+fmin = 100            #Start Frequency (Hz)
+fmax = 10e6            #Stop Frequency (Hz)
+n_pts = 200           #Number of frequency points
+amp = 0.1              #amplitude (V)
+settling_time = 0.01   #Settling time between points
+NPeriods = 32          #Number of period per frequency points
+two_wires = True       #Connection to the DUT mode: True  = 2wires, False = 4wires
 
 output_file_name = "potentiostat_EIS.csv"
 
 BS = bm.BIMMS()
 
 freq, gain_mes, phase_mes = BS.potentiostat_EIS(fmin = fmin, fmax = fmax, n_pts = n_pts, V_amp = amp, V_offset = 0, settling_time = settling_time, NPeriods = NPeriods,
-	differential = False, two_wires = True, coupling = 'DC', apply_cal = True)
+	differential = False, two_wires = two_wires, coupling = 'DC', apply_cal = True)
 
 BS.close()
 
@@ -43,7 +42,6 @@ plt.semilogx(freq,gain_mes)
 plt.xlabel('Frequency ($Hz$)')
 plt.ylabel('Impedance ($\Omega$)')
 plt.grid()
-plt.legend()
 plt.subplot(212)
 plt.semilogx(freq,phase_mes)
 plt.grid()
